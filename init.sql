7fda8e1ef01f9e5c7e87b527262129289a3d7813
Mo-Mansour <Moha1998@outlook.dk>
2026-05-18 14:16:14 +0200

    add full database dump from phpMyAdmin export

--- init.sql
+++ init.sql
@@ -1,114 +1,419 @@
-DROP DATABASE IF EXISTS `WashWorld`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Generation Time: May 18, 2026 at 12:12 PM
+-- Server version: 12.2.2-MariaDB-ubu2404
+-- PHP Version: 8.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `washworld_backend`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `badge`
+--
+
+CREATE TABLE `badge` (
+  `badges_id` char(32) NOT NULL,
+  `badge_label` varchar(20) NOT NULL,
+  `badge_description` varchar(50) NOT NULL,
+  `badge_icon` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
 
 CREATE TABLE `cars` (
-  `car_id` char(32) PRIMARY KEY NOT NULL,
-  `user_id` CHAR(32) NOT NULL,
+  `car_id` char(32) NOT NULL,
+  `user_id` char(32) NOT NULL,
   `car_license_plate` varchar(12) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `favorites_id` char(32) NOT NULL,
+  `user_fk` char(32) NOT NULL,
+  `location_fk` char(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
 
 CREATE TABLE `locations` (
-  `location_id` char(32) PRIMARY KEY NOT NULL,
+  `location_id` char(32) NOT NULL,
   `location_name` varchar(100) NOT NULL,
   `location_address` varchar(255) NOT NULL,
   `location_zipcode` varchar(12) NOT NULL,
   `location_coordinate_x` decimal(9,6) NOT NULL,
   `location_coordinate_y` decimal(9,6) NOT NULL,
   `location_open_hours` varchar(50) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `location_services` (
-  `location_service_id` char(32) PRIMARY KEY NOT NULL,
-  `location_id` char(32),
-  `location_service_status` varchar(50) NOT NULL
-);
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`location_id`, `location_name`, `location_address`, `location_zipcode`, `location_coordinate_x`, `location_coordinate_y`, `location_open_hours`) VALUES
+('1003', 'Fredensborg', 'Hanehovedvej 49', '3300', 12.007447, 55.977559, '7-22'),
+('1004', 'Nykøbing F', 'Guldborgsundcentret 32', '4800', 11.851437, 54.758801, '7-22'),
+('1005', 'Sorø Øst', 'Smedegade 77', '4200', 11.367846, 55.407685, '7-22'),
+('1006', 'Sorø', 'Idagårdsvej 2', '4200', 11.353002, 55.391735, '7-22'),
+('1007', 'Randers NV', 'Udbyhøjvej 7', '8930', 10.054250, 56.466047, '7-22'),
+('1008', 'Ringsted Øst', 'Apotekervej 14', '4180', 11.563255, 55.445137, '7-22'),
+('1009', 'Randers NØ', 'Messingvej 10', '8940', 10.053815, 56.430362, '7-22'),
+('1010', 'Padborg', 'Centerbuen 5', '6270', 8.887800, 54.951505, '7-22'),
+('1011', 'Roskilde', 'Byleddet 2', '4000', 12.109114, 55.643709, '7-22'),
+('1012', 'Hillerød', 'Askelundsvej 8', '3600', 12.074291, 55.845151, '7-22'),
+('1013', 'Haslev', 'Frejasvej 43', '4100', 11.801419, 55.430669, '7-22'),
+('1014', 'Viborg Nord', 'Vognmagervej 21E', '8800', 9.409431, 56.469366, '7-22'),
+('1015', 'Randers', 'Færgevejen 3', '8400', 10.672123, 56.190809, '7-22'),
+('1016', 'Svendborg Vest', 'Odensevej 94', '5700', 10.582398, 55.072950, '7-22'),
+('1017', 'Svendborg', 'Nyborgvej 4', '5700', 10.618592, 55.062893, '7-22'),
+('1018', 'Odense SØ', 'Nyborgvej 343', '5220', 10.435819, 55.391530, '7-22'),
+('1019', 'Haslev Nord', 'Nørregade 70', '4100', 11.790082, 55.451392, '7-22'),
+('1020', 'Tønder', 'Centerpassagen 4', '6400', 9.808034, 54.919430, '7-22'),
+('1021', 'Farum', 'Gammelgårdsvej 84', '3520', 12.370350, 55.816943, '7-22'),
+('1022', 'Næstved', 'Erantisvej 52', '4700', 11.777977, 55.239173, '7-22'),
+('1023', 'Næstved Vest', 'Gammel Holstedvej 1', '4700', 11.782031, 55.249681, '7-22'),
+('1024', 'Middelfart', 'Skovsvinget 27c', '5500', 9.766181, 55.512013, '7-22'),
+('1025', 'Odense', 'Bystævnevej 5', '5200', 10.346525, 55.395026, '7-22'),
+('1026', 'Kolding', 'Vejlevej 132', '6000', 9.458227, 55.504039, '7-22'),
+('1027', 'Ringsted', 'Springstrup 5', '4300', 11.666091, 55.703026, '7-22'),
+('1028', 'Vejle', 'Strevelinsvej 5', '7000', 9.718700, 55.535519, '7-22'),
+('1029', 'Aalborg SØ', 'Gammel Vissevej 1C', '9210', 9.925946, 57.006314, '7-22'),
+('1030', 'Vejle Syd', 'Soldalen 4', '7100', 9.567456, 55.681238, '7-22'),
+('1031', 'Silkeborg', 'Nordre Ringvej 90', '8600', 9.536954, 56.181413, '7-22'),
+('1032', 'Vejle Nord', 'Solkilde Alle 11', '7100', 9.584778, 55.723459, '7-22'),
+('1033', 'Aalborg SV', 'Otto Mønsteds Vej 5', '9200', 9.896256, 57.015248, '7-22'),
+('1034', 'Frederikshavn', 'Sprogøvej 2', '9800', 10.039465, 57.455594, '7-22'),
+('1035', 'Vordingborg', 'Valdemarsgade 57', '4760', 11.910489, 55.010855, '7-22'),
+('1036', 'Nyborg', 'Storebæltsvej 7F', '5800', 10.809624, 55.308498, '7-22'),
+('1037', 'Esbjerg', 'Sædding Ringvej 6', '6710', 8.407419, 55.503728, '7-22'),
+('1038', 'Struer', 'Nybo Bakke 2', '7500', 8.635395, 56.341889, '7-22'),
+('1039', 'Holstebro', 'Dæmningen 21', '7400', 8.959350, 56.132141, '7-22'),
+('1040', 'Tranbjerg', 'Blomstervej 2T', '8381', 10.125000, 56.181787, '7-22'),
+('1041', 'Ikast', 'Vestergårdsvej 3', '7330', 9.103426, 55.960647, '7-22'),
+('1042', 'Viborg Syd', 'Falkevej 25', '8800', 9.388456, 56.444161, '7-22'),
+('1043', 'Hvidovre', 'Gammel Køge Landevej 690', '2660', 12.423950, 55.618231, '7-22'),
+('1044', 'Roskilde Vest', 'Ringstedvej 73', '4000', 12.066559, 55.628427, '7-22'),
+('1045', 'Gentofte', 'Dynamovej 4', '2860', 12.459961, 55.733731, '7-22'),
+('1046', 'Aarhus Nord', 'Lægårdsvej 4', '8520', 10.238525, 56.225669, '7-22'),
+('1047', 'Aarhus', 'Ravnsøvej 48B', '8240', 10.244490, 56.202062, '7-22'),
+('1048', 'Ballerup', 'Industriparken 6', '2750', 12.373295, 55.728714, '7-22'),
+('1049', 'Herlev', 'Nørrelundvej 2', '2730', 12.416697, 55.725365, '7-22'),
+('1050', 'Aarhus Vest', 'Gunnar Clausens Vej 2A', '8260', 10.125033, 56.111373, '7-22'),
+('1051', 'Helsingør', 'Klostermosevej 103', '3000', 12.571863, 56.024018, '7-22'),
+('1052', 'Horsens', 'Vejlevej 102', '8700', 9.804744, 55.833085, '7-22'),
+('1053', 'Ikast Vest', 'Europavej 3', '7430', 9.175422, 56.123699, '7-22'),
+('1054', 'Thisted', 'Østerbakken 111', '7700', 8.735134, 56.968852, '7-22'),
+('1055', 'Slagelse', 'Holbækvej 74', '4400', 11.135830, 55.678767, '7-22'),
+('1056', 'Kolding Nord', 'Vejlevej 251', '6000', 9.454697, 55.513664, '7-22'),
+('1057', 'Skjern', 'Trojels Knæ 6', '6760', 8.780311, 55.351485, '7-22'),
+('1058', 'Køge', 'Københavnsvej 86', '4600', 12.181953, 55.471805, '7-22'),
+('1059', 'Aabenraa', 'Egevej 4', '6200', 9.364450, 55.065643, '7-22'),
+('1060', 'Skanderborg', 'Hesselvang 1', '8500', 10.864451, 56.383895, '7-22'),
+('1061', 'Brønderslev', 'Loftbrovej 2', '9400', 9.969241, 57.089142, '7-22'),
+('1062', 'Hjørring', 'Apholmenvej 9', '9900', 10.519448, 57.462193, '7-22'),
+('1063', 'Ishøj', 'Vejleåvej 19', '2635', 12.321167, 55.623385, '7-22'),
+('1064', 'Haderslev', 'Sverigesvej 2M', '6100', 9.474129, 55.259211, '7-22'),
+('1065', 'Højbjerg', 'Bjødstrupvej 20E', '8270', 10.166967, 56.107525, '7-22'),
+('1066', 'Holstebro Nord', 'Guldborgvej 2-4', '7400', 8.984745, 56.153554, '7-22'),
+('1067', 'Nykøbing Falster', 'Løjtoftevej 6', '4900', 11.149662, 54.832475, '7-22'),
+('1068', 'Frederiksværk', 'Industrivænget 3', '3400', 12.282996, 55.931481, '7-22'),
+('1069', 'Glostrup', 'Roskildevej 376', '2630', 12.294712, 55.658037, '7-22'),
+('1070', 'Viborg', 'Øster Fælled vej 4', '7800', 9.039567, 56.561567, '7-22'),
+('1071', 'Lemvig', 'Bredgade 58', '7600', 8.585535, 56.480435, '7-22'),
+('1072', 'Odense Syd', 'Ørbækvej 99', '5220', 10.433066, 55.379874, '7-22'),
+('1073', 'Vejle Nord', 'Vejlevej 20', '7000', 9.727622, 55.569691, '7-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location_equipment`
+--
 
 CREATE TABLE `location_equipment` (
-  `location_equipment_id` char(32) PRIMARY KEY NOT NULL,
-  `location_id` char(32),
+  `location_equipment_id` char(32) NOT NULL,
+  `location_id` char(32) DEFAULT NULL,
   `location_equipment_type` varchar(50) NOT NULL,
-  `location_equipment_number` int NOT NULL,
+  `location_equipment_number` int(11) NOT NULL,
   `location_equipment_status` varchar(50) NOT NULL,
   `location_equipment_max_height` float NOT NULL,
   `location_equipment_max_width` float NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `subscriptions` (
-  `subscription_id` char(32) PRIMARY KEY NOT NULL,
-  `product_id` char(32),
-  `car_id` char(32),
-  `subscriptions_name` varchar(50) NOT NULL,
-  `subscriptions_price` decimal(10,2) NOT NULL,
-  `subscriptions_status` varchar(20) NOT NULL,
-  `subscriptions_start_date` DATETIME NOT NULL,
-  `subscriptions_end_date` DATETIME NOT NULL,
-  `subscriptions_next_billing_date` DATETIME NOT NULL
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location_services`
+--
+
+CREATE TABLE `location_services` (
+  `location_service_id` char(32) NOT NULL,
+  `location_id` char(32) DEFAULT NULL,
+  `location_service_status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offers`
+--
+
+CREATE TABLE `offers` (
+  `offer_id` char(32) NOT NULL,
+  `product_id` char(32) DEFAULT NULL,
+  `offer_description` varchar(255) NOT NULL,
+  `offer_discount_percentage` decimal(5,2) NOT NULL,
+  `offer_start_date` datetime NOT NULL,
+  `offer_end_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
 
 CREATE TABLE `products` (
-  `product_id` char(32) PRIMARY KEY NOT NULL,
+  `product_id` char(32) NOT NULL,
   `product_name` varchar(50) NOT NULL,
   `product_price` decimal(10,2) NOT NULL,
   `product_category` varchar(100) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscriptions`
+--
+
+CREATE TABLE `subscriptions` (
+  `subscription_id` char(32) NOT NULL,
+  `product_id` char(32) DEFAULT NULL,
+  `car_id` char(32) DEFAULT NULL,
+  `subscriptions_name` varchar(50) NOT NULL,
+  `subscriptions_price` decimal(10,2) NOT NULL,
+  `subscriptions_status` varchar(20) NOT NULL,
+  `subscriptions_start_date` datetime NOT NULL,
+  `subscriptions_end_date` datetime NOT NULL,
+  `subscriptions_next_billing_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
-  `user_id` char(32) PRIMARY KEY NOT NULL,
+  `user_id` char(32) NOT NULL,
   `user_email` varchar(100) NOT NULL,
   `user_password_hashed` varchar(255) NOT NULL,
   `user_firstname` varchar(50) NOT NULL,
   `user_lastname` varchar(50) NOT NULL,
   `user_phone` varchar(20) NOT NULL,
-  `user_created_at` DATETIME NOT NULL,
-  `user_updated_at` DATETIME NOT NULL,
-  `user_deleted_at` DATETIME,
+  `user_created_at` datetime NOT NULL,
+  `user_updated_at` datetime NOT NULL,
+  `user_deleted_at` datetime DEFAULT NULL,
   `user_verification_key` char(32) NOT NULL,
-  `user_verified_at` DATETIME NOT NULL,
-  `user_reset_password` char(75)
-);
+  `user_verified_at` datetime NOT NULL,
+  `user_reset_password` char(75) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-CREATE TABLE `user_credits` (
-  `user_credit_id` char(32) PRIMARY KEY NOT NULL,
-  `user_credit_amount` decimal(10,2) NOT NULL,
-  `user_id` char(32),
-  `product_id` char(32)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `offers` (
-  `offer_id` char(32) PRIMARY KEY NOT NULL,
-  `product_id` char(32),
-  `offer_description` varchar(255) NOT NULL,
-  `offer_discount_percentage` decimal(5,2) NOT NULL,
-  `offer_start_date` DATETIME NOT NULL,
-  `offer_end_date` DATETIME NOT NULL
-);
+--
+-- Table structure for table `user_badges`
+--
+
+CREATE TABLE `user_badges` (
+  `user_badges_id` char(32) NOT NULL,
+  `user_fk` char(32) NOT NULL,
+  `badges_fk` char(32) NOT NULL,
+  `achieved` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wash_log`
+--
 
 CREATE TABLE `wash_log` (
-  `wash_log_id` char(32) PRIMARY KEY NOT NULL,
-  `car_id` char(32),
-  `product_id` char(32),
-  `location_id` char(32),
-  `wash_log_start_time` DATETIME NOT NULL
-);
+  `wash_log_id` char(32) NOT NULL,
+  `car_id` char(32) DEFAULT NULL,
+  `product_id` char(32) DEFAULT NULL,
+  `location_id` char(32) DEFAULT NULL,
+  `wash_log_start_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
-ALTER TABLE `cars` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-ALTER TABLE `cars` ADD UNIQUE (`car_license_plate`);
+--
+-- Indexes for dumped tables
+--
 
-ALTER TABLE `users` ADD UNIQUE (`user_email`);
+--
+-- Indexes for table `badge`
+--
+ALTER TABLE `badge`
+  ADD PRIMARY KEY (`badges_id`);
 
-ALTER TABLE `location_services` ADD FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`car_id`),
+  ADD UNIQUE KEY `car_license_plate` (`car_license_plate`),
+  ADD KEY `user_id` (`user_id`);
 
-ALTER TABLE `location_equipment` ADD FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`favorites_id`),
+  ADD UNIQUE KEY `user_fk` (`user_fk`,`location_fk`);
 
-ALTER TABLE `subscriptions` ADD FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`);
-ALTER TABLE `subscriptions` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`location_id`);
 
-ALTER TABLE `user_credits` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
-ALTER TABLE `user_credits` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+--
+-- Indexes for table `location_equipment`
+--
+ALTER TABLE `location_equipment`
+  ADD PRIMARY KEY (`location_equipment_id`),
+  ADD KEY `location_id` (`location_id`);
 
-ALTER TABLE `offers` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+--
+-- Indexes for table `location_services`
+--
+ALTER TABLE `location_services`
+  ADD PRIMARY KEY (`location_service_id`),
+  ADD KEY `location_id` (`location_id`);
 
-ALTER TABLE `wash_log` ADD FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`);
-ALTER TABLE `wash_log` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
-ALTER TABLE `wash_log` ADD FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+--
+-- Indexes for table `offers`
+--
+ALTER TABLE `offers`
+  ADD PRIMARY KEY (`offer_id`),
+  ADD KEY `product_id` (`product_id`);
 
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
 
+--
+-- Indexes for table `subscriptions`
+--
+ALTER TABLE `subscriptions`
+  ADD PRIMARY KEY (`subscription_id`),
+  ADD KEY `car_id` (`car_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
+
+--
+-- Indexes for table `user_badges`
+--
+ALTER TABLE `user_badges`
+  ADD PRIMARY KEY (`user_badges_id`),
+  ADD KEY `badges_user_fk` (`user_fk`),
+  ADD KEY `badges_badges_fk` (`badges_fk`);
+
+--
+-- Indexes for table `wash_log`
+--
+ALTER TABLE `wash_log`
+  ADD PRIMARY KEY (`wash_log_id`),
+  ADD KEY `car_id` (`car_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cars`
+--
+ALTER TABLE `cars`
+  ADD CONSTRAINT `1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `location_equipment`
+--
+ALTER TABLE `location_equipment`
+  ADD CONSTRAINT `1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+
+--
+-- Constraints for table `location_services`
+--
+ALTER TABLE `location_services`
+  ADD CONSTRAINT `1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+
+--
+-- Constraints for table `offers`
+--
+ALTER TABLE `offers`
+  ADD CONSTRAINT `1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+--
+-- Constraints for table `subscriptions`
+--
+ALTER TABLE `subscriptions`
+  ADD CONSTRAINT `1` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`),
+  ADD CONSTRAINT `2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+--
+-- Constraints for table `user_badges`
+--
+ALTER TABLE `user_badges`
+  ADD CONSTRAINT `badges_badges_fk` FOREIGN KEY (`badges_fk`) REFERENCES `badge` (`badges_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `badges_user_fk` FOREIGN KEY (`user_fk`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `wash_log`
+--
+ALTER TABLE `wash_log`
+  ADD CONSTRAINT `1` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`),
+  ADD CONSTRAINT `2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+  ADD CONSTRAINT `3` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
